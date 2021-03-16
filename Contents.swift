4557828d39d337b415a04be76f863703a21e60bc
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-03-16 13:37:02 +0700

    Hari 1 = Variable, Integer dan Multiline String

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
+var nama = "Mufti"
+nama = "Uwais"
+
+// Integer
+var age = 21
+age = 22
+
+var uangJajan = 1_000_000
+
+// multiline string
+var pantun = "Buah rambutan Buah mangga Kamu serabutan Saya bangga"
+print(pantun)
+
+
+
+
