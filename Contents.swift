9441b4410c8cb68320ffde553e9f38de7a5099fc
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-03-22 13:51:58 +0700

    Hari kedua (part 1)

--- Contents.swift
+++ Contents.swift
@@ -11,8 +11,26 @@ var uangJajan = 1_000_000
 
 // multiline string
 var pantun = "Buah rambutan Buah mangga Kamu serabutan Saya bangga"
-print(pantun)
 
+// constants
+let name = "Dandy"
 
+// type annotations
+var album: String = "Reputation"
+var year: Int = 1989
+var height: Double = 1.78
+var taylorRocks: Bool = true
+
+// array
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+// set
+let colors = ["red", "green", "blue", "red", "blue"]
+print(colors)
 
 
